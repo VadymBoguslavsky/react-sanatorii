dd2bdb6bc156564487fdb5a360fe0e23d38cd134
Vadim <axixe92@gmail.com>
2019-05-14 17:11:00 +0300

    gitignore

---
+++ build/precache-manifest.4f212d32b8c12b087c66087bbd403be3.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d82e0023a35c5aae442be6ff8fc536",
+    "url": "/react-sanatorii/index.html"
+  },
+  {
+    "revision": "1fc80db46b69091af2ce",
+    "url": "/react-sanatorii/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6e0a4d76e3c4863e89ec",
+    "url": "/react-sanatorii/static/css/main.44db5d81.chunk.css"
+  },
+  {
+    "revision": "1fc80db46b69091af2ce",
+    "url": "/react-sanatorii/static/js/2.0a6ba3e8.chunk.js"
+  },
+  {
+    "revision": "6e0a4d76e3c4863e89ec",
+    "url": "/react-sanatorii/static/js/main.733386f7.chunk.js"
+  },
+  {
+    "revision": "c28152219bdc9a24c048",
+    "url": "/react-sanatorii/static/js/runtime~main.9e8b4d35.js"
+  },
+  {
+    "revision": "5d75eb50fe4917c67749b65426a3d27d",
+    "url": "/react-sanatorii/static/media/1 (10)-min.5d75eb50.jpg"
+  },
+  {
+    "revision": "bd1165d095021c8c680c9186f35bec8d",
+    "url": "/react-sanatorii/static/media/1 (11)-min.bd1165d0.jpg"
+  },
+  {
+    "revision": "02f337e22f7be115cb26cbbbc9aa0207",
+    "url": "/react-sanatorii/static/media/1 (12)-min.02f337e2.jpg"
+  },
+  {
+    "revision": "8f6a3a5872af11527d87ac20b169e37a",
+    "url": "/react-sanatorii/static/media/1 (13)-min.8f6a3a58.jpg"
+  },
+  {
+    "revision": "1cb1fb1de7809f823088be8bafae8f0a",
+    "url": "/react-sanatorii/static/media/1 (14)-min.1cb1fb1d.jpg"
+  },
+  {
+    "revision": "ec59d12c34a53c14814c576fc00cdbee",
+    "url": "/react-sanatorii/static/media/1 (15)-min.ec59d12c.jpg"
+  },
+  {
+    "revision": "3e2261a5d5307b7b01166ad48955dda4",
+    "url": "/react-sanatorii/static/media/1 (16)-min.3e2261a5.jpg"
+  },
+  {
+    "revision": "dd2803ac0d390ccff88db813cd44917b",
+    "url": "/react-sanatorii/static/media/1 (17)-min.dd2803ac.jpg"
+  },
+  {
+    "revision": "36d02aabce9d6d30f472b5b97fa4a75f",
+    "url": "/react-sanatorii/static/media/1 (18)-min.36d02aab.jpg"
+  },
+  {
+    "revision": "2738abc1037103af05422a7f131314e0",
+    "url": "/react-sanatorii/static/media/1 (19)-min.2738abc1.jpg"
+  },
+  {
+    "revision": "c30413f31990edc17a77588e738af2ca",
+    "url": "/react-sanatorii/static/media/1 (2)-min.c30413f3.jpg"
+  },
+  {
+    "revision": "58726e36a4132ab2af65f818ecd55244",
+    "url": "/react-sanatorii/static/media/1 (20)-min.58726e36.jpg"
+  },
+  {
+    "revision": "30873965d1a3b21a628bd282626536ba",
+    "url": "/react-sanatorii/static/media/1 (21)-min.30873965.jpg"
+  },
+  {
+    "revision": "629f01df68f7e3bd26b194ef030bcde3",
+    "url": "/react-sanatorii/static/media/1 (22)-min.629f01df.jpg"
+  },
+  {
+    "revision": "1cfaab3aea7351aa11a06213b1caa0a1",
+    "url": "/react-sanatorii/static/media/1 (23)-min.1cfaab3a.jpg"
+  },
+  {
+    "revision": "53b2ceb63707d872ff5fc2873b1cf1c2",
+    "url": "/react-sanatorii/static/media/1 (24)-min.53b2ceb6.jpg"
+  },
+  {
+    "revision": "a5e3458d6e686e8f4d51edd0cd81dbc2",
+    "url": "/react-sanatorii/static/media/1 (25)-min.a5e3458d.jpg"
+  },
+  {
+    "revision": "e8f95c2a25d55c230a70eba82f5342be",
+    "url": "/react-sanatorii/static/media/1 (26)-min.e8f95c2a.jpg"
+  },
+  {
+    "revision": "953e225f79f351add43c56e833146ed1",
+    "url": "/react-sanatorii/static/media/1 (27)-min.953e225f.jpg"
+  },
+  {
+    "revision": "185f2b5ef89c45866a7d1d07dc832387",
+    "url": "/react-sanatorii/static/media/1 (28)-min.185f2b5e.jpg"
+  },
+  {
+    "revision": "f059101497aec7c8929234a9e466fc82",
+    "url": "/react-sanatorii/static/media/1 (29)-min.f0591014.jpg"
+  },
+  {
+    "revision": "abf7010cb81cd2bbcdcc0997742f06b0",
+    "url": "/react-sanatorii/static/media/1 (3)-min.abf7010c.jpg"
+  },
+  {
+    "revision": "fafb938f71806be88beff0dc45857f0b",
+    "url": "/react-sanatorii/static/media/1 (30)-min.fafb938f.jpg"
+  },
+  {
+    "revision": "983dbf0f0092d74c446add67d1face51",
+    "url": "/react-sanatorii/static/media/1 (31)-min.983dbf0f.jpg"
+  },
+  {
+    "revision": "e445c460a4e644addacdb015de98b91f",
+    "url": "/react-sanatorii/static/media/1 (32)-min.e445c460.jpg"
+  },
+  {
+    "revision": "d76b1eb39589a0db8034b9cf1b3cad8f",
+    "url": "/react-sanatorii/static/media/1 (33)-min.d76b1eb3.jpg"
+  },
+  {
+    "revision": "7ea6572d444148567c24ff72497ac28a",
+    "url": "/react-sanatorii/static/media/1 (34)-min.7ea6572d.jpg"
+  },
+  {
+    "revision": "7718cca64426f2df2b09362defba3fa9",
+    "url": "/react-sanatorii/static/media/1 (35)-min.7718cca6.jpg"
+  },
+  {
+    "revision": "dd59795a5a241924bfcb2dd152f93ccc",
+    "url": "/react-sanatorii/static/media/1 (36)-min.dd59795a.jpg"
+  },
+  {
+    "revision": "b45b001fc72af0c213b6ab8c71f11210",
+    "url": "/react-sanatorii/static/media/1 (4)-min.b45b001f.jpg"
+  },
+  {
+    "revision": "2f023f3b17a4ad60e78ee50bf4f426b7",
+    "url": "/react-sanatorii/static/media/1 (5)-min.2f023f3b.jpg"
+  },
+  {
+    "revision": "00ca335835123e71aaf5e4cf323403bf",
+    "url": "/react-sanatorii/static/media/1 (6)-min.00ca3358.jpg"
+  },
+  {
+    "revision": "80c18099306f463d9147ddb28c7aea80",
+    "url": "/react-sanatorii/static/media/1 (7)-min.80c18099.jpg"
+  },
+  {
+    "revision": "34c128f013460ff2097c66510c1fda4a",
+    "url": "/react-sanatorii/static/media/1 (8)-min.34c128f0.jpg"
+  },
+  {
+    "revision": "499b1025619b69696fc5502ee982bff1",
+    "url": "/react-sanatorii/static/media/1 (9)-min.499b1025.jpg"
+  },
+  {
+    "revision": "3e2261a5d5307b7b01166ad48955dda4",
+    "url": "/react-sanatorii/static/media/1-min.3e2261a5.jpg"
+  },
+  {
+    "revision": "09923d9af745f0622f32caf7dccba3dc",
+    "url": "/react-sanatorii/static/media/about-min.09923d9a.jpg"
+  },
+  {
+    "revision": "32a19cca27e7ea8f0fa38193cc17e207",
+    "url": "/react-sanatorii/static/media/phonecall.32a19cca.gif"
+  },
+  {
+    "revision": "3c66bb68d301009bca228bfff02e589f",
+    "url": "/react-sanatorii/static/media/vodaphone-min.3c66bb68.png"
+  }
+]);
