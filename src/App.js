709cafd311ad1dc1b7d1589b85478c0b9944baa3
Vadim <axixe92@gmail.com>
2019-05-14 11:09:16 +0300

    Add normalise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
 import About from './components/about/about.js';
 import Header from './components/header/header.js'
 import SimpleSlider from './components/slider/slider.js'
 import Footer from './components/footer/footer.js'
+
+import 'normalize.css';
 
 function App() {
   return (
