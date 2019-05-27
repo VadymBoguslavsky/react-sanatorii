d1ca344731150f69ee862ba9fddff238504e1008
Vadim <axixe92@gmail.com>
2019-05-27 13:05:47 +0300

    improvments for phone visual

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,18 +4,29 @@ class Header extends Component {
 
   state = {
     showPhones: false
+  };
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
   }
 
-showPhonesOnClick = (e) => {
-  this.setState({
-    showPhones: !this.state.showPhones
-  })
-}
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
 
-render(){
+  myRef = React.createRef();
 
-  return (
-      <div id="call_us" onClick={this.showPhonesOnClick}>
+  handleClickOutside = e => {
+    if (!this.myRef.current.contains(e.target)) {
+      this.setState({ showPhones: false });
+    }
+  };
+
+  handleClickInside = () => this.setState({ showPhones: true });
+
+  render() {
+    return (
+      <div id="call_us" ref={this.myRef} onClick={this.handleClickInside}>
         {this.state.showPhones ?
           <div id="myDIV">
             <div className="phone__number">
@@ -25,9 +36,9 @@ render(){
               <a href="tel:+38 (067) 75 76 844">+38 (067) 75 76 844</a>
               <a href="tel:(032) 47 59 805">(032) 47 59 805</a>
             </div>
-          </div> : ''}
-        </div>
-      );
+          </div> : " "}
+      </div>
+    );
   }
 }
 
